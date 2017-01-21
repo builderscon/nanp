2503534fa4da7d2bff79953b975d7b1bb7409533
janus_wel <janus.wel.3@gmail.com>
2017-01-21 13:52:01 +0900

    :sparkles: add component to read QR code

---
+++ component.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  Alert,
+} from 'react-native'
+import Camera from 'react-native-camera'
+
+const styles = StyleSheet.create({
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  },
+})
+
+export default class Reader extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      readData: '',
+    }
+  }
+
+  onQrCodeRead(event) {
+    this.setState({
+      readData: event.data.toString()
+    })
+    Alert.alert('read')
+  }
+
+  render() {
+    return (
+      <Camera
+        style={styles.preview}
+        aspect={Camera.constants.Aspect.fill}
+        onBarCodeRead={event => {this.onQrCodeRead(event)}}
+      >
+        <Text>{this.state.readData}</Text>
+      </Camera>
+    )
+  }
+}
